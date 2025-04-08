b4379440f9ffefe28af77579c9be45db7904ea9b
ilakkiyan-uruthireswaran-ics4u1-3-25 <Ilakkiyan.uruthireswaran@student.tdsb.on.ca>
2025-04-08 21:34:13 +0000

    passed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,27 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void insertionSort(char[] characters) {
+		for (int i = 0; i < characters.length; i++) {
+			char key = characters[i];
+			int j = i - 1;
+			while (j >= 0 && characters[j] > key) {
+				characters[j + 1] = characters[j];
+				j--;
+			}
+			characters[j + 1] = key;
+		}
+	}
 
+	public static void main(String args[]) {
+		char[] letters = {'a', 'D', 'v', 'V', 'g'};
+		insertionSort(letters);
+		for (char c : letters) {
+			System.out.println(c + " ");
+		}
+		}
+		
+		
 	}
 
 
-}
+
